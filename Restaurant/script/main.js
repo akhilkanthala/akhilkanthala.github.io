87b8b5ab3a598bb2b0713bcfc443cf886a3853b0
Akhil Kanthala <akhil.kanthala@zemosolabs.com>
2019-09-09 13:43:09 +0530

    javascript2

--- Restaurant/script/main.js
+++ Restaurant/script/main.js
@@ -131,7 +131,8 @@ function handleEnd(ev,i) {
  }
  function fun(i){
      var items=JSON.parse(localStorage.getItem("table-"+i+""));
- //    document.getElementById('target'+i+'').style.backgroundColor="yellow";
+     document.getElementById('clos').innerHTML=`<button type="button" class="close" onclick="col(`+i+`)" data-dismiss="modal">&times;</button><h4 class="modal-title" id="modal-title"></h4>`;
+     document.getElementById('target'+i+'').style.backgroundColor="yellow";
      if(items!=null){
          var total=0;     
     htm='Table-'+i+'|Order Details';
@@ -218,6 +219,6 @@ function myFunction() {
         }          
     }
   }
-  function col(){
-
+  function col(i){
+    document.getElementById('target'+i+'').style.backgroundColor="white";
   }
